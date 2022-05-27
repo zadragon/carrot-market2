895e34bc72cb1d948cf436319c2963732966a8d6
zayongLee <mase0804@naver.com>
2022-05-27 15:57:31 +0900

    update

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,4 +1,3 @@
-import {withIronSessionApiRoute} from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
@@ -8,25 +7,23 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-    const { token } = req.body;
-    console.log(token);
-    
-    const foundToken = await client.token.findUnique({
-      where:{
-        payload:token
-      },
-    });
-    if(!foundToken) return res.status(404).end();
-    req.session.user = {
-      id:foundToken.userId
-    }
-    await req.session.save();
-    await client.token.deleteMany({
-      where:{
-        userId:foundToken.userId
-      }
-    })
-    res.json({ok:true})
+  const { token } = req.body;
+  const foundToken = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!foundToken) return res.status(404).end();
+  req.session.user = {
+    id: foundToken.userId,
+  };
+  await req.session.save();
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId,
+    },
+  });
+  res.json({ ok: true });
 }
 
 export default withApiSession(withHandler("POST", handler));
