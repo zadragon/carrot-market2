64d8d88e611809de3708f0219b2fb41fcf82017c
zayongLee <mase0804@naver.com>
2022-06-03 18:30:45 +0900

    12.0 할차례

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -10,7 +10,15 @@ async function handler(
 ) {
 
   if(req.method === "GET"){
-      const products = await client.product.findMany({})
+      const products = await client.product.findMany({
+        include:{
+          _count:{
+            select:{
+              favs:true
+            }
+          }
+        }
+      })
       res.json({
         ok:true,
         products,
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -2,34 +2,37 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import { useRouter } from "next/router";
-import useSWR from "swr";
-import products from "pages/api/products";
+import useSWR, { useSWRConfig } from "swr";
 import Link from "next/link";
 import { Product, User } from "@prisma/client";
-import useMutation from '@libs/client/useMutation';
-import { cls } from '../../libs/client/utils';
+import useMutation from "@libs/client/useMutation";
+import { cls } from "@libs/client/utils";
+import useUser from "@libs/client/useUser";
 
-interface ProductWithUser extends Product{
-  user:User
+interface ProductWithUser extends Product {
+  user: User;
 }
-
-interface ItemDetailResponse{
-  ok:boolean;
-  product:ProductWithUser;
-  relatedProducts:Product[];
-  isLiked:boolean;
+interface ItemDetailResponse {
+  ok: boolean;
+  product: ProductWithUser;
+  relatedProducts: Product[];
+  isLiked: boolean;
 }
 
 const ItemDetail: NextPage = () => {
+  const { user, isLoading } = useUser();
   const router = useRouter();
-  const {data} = useSWR<ItemDetailResponse>(
+  const { mutate } = useSWRConfig();
+  const { data, mutate: boundMutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   );
   const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
   const onFavClick = () => {
+    if (!data) return;
+    boundMutate((prev) => prev && { ...prev, isLiked: !prev.isLiked }, false);
+    // mutate("/api/users/me", (prev: any) => ({ ok: !prev.ok }), false);
     toggleFav({});
-  }
-  
+  };
   return (
     <Layout canGoBack>
       <div className="px-4  py-4">
@@ -38,7 +41,9 @@ const ItemDetail: NextPage = () => {
           <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
             <div className="w-12 h-12 rounded-full bg-slate-300" />
             <div>
-              <p className="text-sm font-medium text-gray-700">{data?.product?.user?.name}</p>
+              <p className="text-sm font-medium text-gray-700">
+                {data?.product?.user?.name}
+              </p>
               <Link href={`/users/profiles/${data?.product?.user?.id}`}>
                 <a className="text-xs font-medium text-gray-500">
                   View profile &rarr;
@@ -47,17 +52,22 @@ const ItemDetail: NextPage = () => {
             </div>
           </div>
           <div className="mt-5">
-            <h1 className="text-3xl font-bold text-gray-900">{data?.product?.name}</h1>
-            <span className="text-2xl block mt-3 text-gray-900">${data?.product?.price}</span>
+            <h1 className="text-3xl font-bold text-gray-900">
+              {data?.product?.name}
+            </h1>
+            <span className="text-2xl block mt-3 text-gray-900">
+              ${data?.product?.price}
+            </span>
             <p className=" my-6 text-gray-700">{data?.product?.description}</p>
             <div className="flex items-center justify-between space-x-2">
               <Button large text="Talk to seller" />
-              <button 
-                onClick={onFavClick} 
-
+              <button
+                onClick={onFavClick}
                 className={cls(
-                  "p-3 rounded-md flex items-center justify-center hover:bg-gray-100",
-                  data?.isLiked ? "text-red-400 hover:text-red-500" : "text-gray-400 hover:text-gray-500"
+                  "p-3 rounded-md flex items-center hover:bg-gray-100 justify-center ",
+                  data?.isLiked
+                    ? "text-red-500  hover:text-red-600"
+                    : "text-gray-400  hover:text-gray-500"
                 )}
               >
                 {data?.isLiked ? (
@@ -90,8 +100,6 @@ const ItemDetail: NextPage = () => {
                     />
                   </svg>
                 )}
-
-                
               </button>
             </div>
           </div>
@@ -99,11 +107,13 @@ const ItemDetail: NextPage = () => {
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
           <div className=" mt-6 grid grid-cols-2 gap-4">
-            {data?.relatedProducts.map((product) => (
+            {data?.relatedProducts?.map((product) => (
               <div key={product.id}>
                 <div className="h-56 w-full mb-4 bg-slate-300" />
                 <h3 className="text-gray-700 -mb-1">{product.name}</h3>
-                <span className="text-sm font-medium text-gray-900">${product.price}</span>
+                <span className="text-sm font-medium text-gray-900">
+                  ${product.price}
+                </span>
               </div>
             ))}
           </div>
