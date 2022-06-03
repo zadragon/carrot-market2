ed4fd5db596d3f47b818ce06814f8e73c3c42221
zayongLee <mase0804@naver.com>
2022-06-03 15:53:05 +0900

    save

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -7,7 +7,10 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { id } = req.query;
+  const { 
+      query:{id}, 
+      session:{user}
+    } = req;
   const product = await client.product.findUnique({
     where: {
       id: +id.toString(),
@@ -22,12 +25,12 @@ async function handler(
       },
     },
   });
+
   const terms = product?.name.split(" ").map((word) => ({
     name: {
       contains: word,
     },
   }));
-  console.log(terms);
   
   const relatedProducts = await client.product.findMany({
     where: {
@@ -39,9 +42,19 @@ async function handler(
       },
     },
   });
-  console.log(relatedProducts);
-  
-  res.json({ ok: true, product, relatedProducts });
+
+  const isLiked = Boolean(
+    await client.fav.findFirst({
+        where:{
+            productId:product?.id,
+            userId:user?.id,
+        },
+        select:{
+            id:true,
+        }
+    })
+  )
+  res.json({ ok: true, product, isLiked, relatedProducts });
 }
 
 export default withApiSession(
