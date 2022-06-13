c260e12bf9835a3492ca37097ea713f354ac3985
zayongLee <mase0804@naver.com>
2022-06-13 14:20:32 +0900

    merge

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -4,6 +4,7 @@ import FloatingButton from "@components/floating-button";
 import Layout from "@components/layout";
 import useSWR from 'swr';
 import { Post, User } from "@prisma/client";
+import useCoords from "@libs/client/useCoords";
 
 interface PostWithUser extends Post {
   user:User;
@@ -18,8 +19,12 @@ interface PostsResponse{
   posts: PostWithUser[]
 }
 const Community: NextPage = () => {
-  const {data} = useSWR<PostsResponse>(`/api/posts`);
-  console.log(data);
+  const {latitude, longitude} = useCoords();
+  const {data} = useSWR<PostsResponse>(
+    latitude && longitude 
+      ? `/api/posts?latitude=${latitude}&longitude=${longitude}` 
+      : null
+  );
   
   return (
     <Layout hasTabBar title="동네생활">
