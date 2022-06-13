8f63cb09ac8eab6350c4cd5b3bbf56a0827dd7ff
zayongLee <mase0804@naver.com>
2022-06-13 17:07:25 +0900

    13.6 볼차례

--- libs/client/useUser.tsx
+++ libs/client/useUser.tsx
@@ -1,16 +1,20 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import userSWR from "swr";
+import { User } from "@prisma/client";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import useSWR from "swr";
 
+interface ProfileResponse {
+  ok: boolean;
+  profile: User;
+}
 
-
-export default function useUser(){
-    const {data, error} = userSWR("/api/users/me")
-    const router = useRouter();
-    useEffect(()=>{
-        if(data && !data.ok){
-            router.replace("/enter");
-        }
-    },[data, router])
-    return {user:data?.profile, isLoading:!data && !error};
+export default function useUser() {
+  const { data, error } = useSWR<ProfileResponse>("/api/users/me");
+  const router = useRouter();
+  useEffect(() => {
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
+  return { user: data?.profile, isLoading: !data && !error };
 }
