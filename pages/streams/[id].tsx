0f394b3171d86090f7d612824b003ecacb54178c
zadragon <mase0804@naver.com>
2022-06-20 00:43:02 +0900

    new

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -6,50 +6,83 @@ import { useRouter } from "next/router";
 import { Stream } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import useMutation from "@libs/client/useMutation";
+import useUser from "@libs/client/useUser";
+import { useEffect } from "react";
+
+interface StreamMessage {
+  message: string;
+  id: number;
+  user: {
+    avatar?: string;
+    id: number;
+  };
+}
+
+interface StreamWithMessages extends Stream {
+  messages: StreamMessage[];
+}
 
 interface StreamResponse {
-  ok:true;
-  stream:Stream
+  ok: true;
+  stream: StreamWithMessages;
 }
 
-interface MessageForm{
-  message:string;
-
+interface MessageForm {
+  message: string;
 }
 
-const Stream: NextPage = () => {
+const StreamNew: NextPage = () => {
+  const { user } = useUser();
   const router = useRouter();
-  const {register, handleSubmit, reset} = useForm<MessageForm>();
-  const {data} = useSWR<StreamResponse>(router.query.id ? `/api/streams/${router.query.id}` : null);
-  const [sendMessage,{loading, data:sendMessageData}] = useMutation(`/api/streams/${router.query.id}/messages`)
-  const onValid = (form:MessageForm) => {
-    if(loading) return;
+  const { register, handleSubmit, reset } = useForm<MessageForm>();
+  const { data, mutate } = useSWR<StreamResponse>(
+    router.query.id ? `/api/streams/${router.query.id}` : null
+  );
+  const [sendMessage, { loading, data: sendMessageData }] = useMutation(
+    `/api/streams/${router.query.id}/messages`
+  );
+  const onValid = (form: MessageForm) => {
+    if (loading) return;
     reset();
-    sendMessage(form)
-  }
+    sendMessage(form);
+  };
+  useEffect(() => {
+    if (sendMessageData && sendMessageData.ok) {
+      mutate();
+    }
+  }, [sendMessageData, mutate]);
   return (
     <Layout canGoBack>
       <div className="py-10 px-4  space-y-4">
         <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
         <div className="mt-5">
-          <h1 className="text-3xl font-bold text-gray-900">{data?.stream?.name}</h1>
-          <span className="text-2xl block mt-3 text-gray-900">{data?.stream?.price}</span>
-          <p className=" my-6 text-gray-700">
-            {data?.stream?.description}
-          </p>
+          <h1 className="text-3xl font-bold text-gray-900">
+            {data?.stream?.name}
+          </h1>
+          <span className="text-2xl block mt-3 text-gray-900">
+            ${data?.stream?.price}
+          </span>
+          <p className=" my-6 text-gray-700">{data?.stream?.description}</p>
         </div>
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Live Chat</h2>
           <div className="py-10 pb-16 h-[50vh] overflow-y-scroll  px-4 space-y-4">
-            <Message message="Hi how much are you selling them for?" />
-            <Message message="I want ￦20,000" reversed />
-            <Message message="미쳤어" />
+            {data?.stream.messages.map((message) => (
+              <Message
+                key={message.id}
+                message={message.message}
+                reversed={message.user.id === user?.id}
+              />
+            ))}
           </div>
           <div className="fixed py-2 bg-white  bottom-0 inset-x-0">
-            <form onSubmit={handleSubmit(onValid)} className="flex relative max-w-md items-center  w-full mx-auto">
+            <form
+              onSubmit={handleSubmit(onValid)}
+              className="flex relative max-w-md items-center  w-full mx-auto"
+            >
               <input
-                {...register("message",{required:true})}
                 type="text"
+                {...register("message", { required: true })}
                 className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
               />
               <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
@@ -65,4 +98,4 @@ const Stream: NextPage = () => {
   );
 };
 
-export default Stream;
+export default StreamNew;
